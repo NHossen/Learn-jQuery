ef9c41a4caccd4f81abdc118f3180411de09709c
NHossen <naeemhossen2509@gmail.com>
2023-08-09 19:26:05 +0400

    by jQuery learn how to added class by jQuery

--- learn.js
+++ learn.js
@@ -2,5 +2,12 @@
 
 // Text Color..
 // $('body').css('background','gray')
+// Class added by jquery
 $('h1').css('color','green');$('h1').addClass('big-title margin-50');
 $('button').css('color','red');
+
+// Text manupulating by jQuery
+
+$('p').text('Naeem Hossen').css('color','red').css('fontSize','100px');
+
+$('button').html('<h4>hey this is naeem from united arab emirates </h4>').css('color','blue').css('marginTop','100px')
