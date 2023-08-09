303b6585c0b6856a47a4ef97cd275765bc3772e3
NHossen <naeemhossen2509@gmail.com>
2023-08-09 21:54:01 +0400

    by jQuery added event listeners

--- learn.js
+++ learn.js
@@ -10,7 +10,42 @@ $('button').css('color','red');
 
 $('p').text('Naeem Hossen').css('color','red').css('fontSize','100px');
 
-$('button').html('<h4>hey this is naeem from united arab emirates </h4>').css('color','blue').css('marginTop','100px');
+$('button').html('<h4>hey </h4>').css('color','blue').css('marginBottom','100px');
+
+// Set attribute and change
 
 $('img').attr('src');
 $('a').attr('href','https://bing.com');
+
+// Event listeners //I think event listener like hover
+$('p').click(function(){
+
+    $('p').css('color','blue').text('Good Morning from NH');
+
+});
+
+// Vanila Javascript
+
+// for(var i=0;i<5;i++){
+//     document.querySelectorAll('button')[i].addEventListener('click',function(){
+//         document.querySelector('h1').style.color='purple';
+//     })
+// }
+
+// jQuery
+$('button').click(function(){
+    $('h1').css('color','purple');
+});
+
+// Adding text by keypress by jQuery 
+
+// $(document).keypress(function(event){
+//     $('h1').text(event.key);
+// })
+
+
+// Another option to add a event listeners //neeed to learn dom event and method
+
+$(document).on('mouseover',function(){
+    $('p').css('color','green').text('Good Morning from NH');
+})
